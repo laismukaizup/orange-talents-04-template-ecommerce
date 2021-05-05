4e2efcecf72f928a288ee31c01af24aa97edc718
Lais Mukai <lais.mukai@zup.com.br>
2021-05-04 22:23:39 -0300

    acertos de nomenclatura

--- src/main/java/br/com/academy/lais/mercadolivre/Hanking/HankingController.java
+++ src/main/java/br/com/academy/lais/mercadolivre/Hanking/HankingController.java
@@ -1,6 +1,5 @@
 package br.com.academy.lais.mercadolivre.Hanking;
 
-import br.com.academy.lais.mercadolivre.NotaFiscal.NotaFiscalRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/notafiscal")
+@RequestMapping("/hanking")
 public class HankingController {
     @PostMapping()
     public String cadastrar(@RequestBody @Valid HankingRequest hankingRequest) {
